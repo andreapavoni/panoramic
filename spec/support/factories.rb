679b4acd190f2bb6dc8c15e5440240201b147273
fastjames <fastjames@gmail.com>
2018-10-03 23:18:18 -0500

    Update static factory attributes to dynamic
    factory_bot will no longer support static attributes in the next version
    so this resolves a deprecation warning.

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,11 +1,11 @@
 # FactoryBot.define :database_template do |t|
 FactoryBot.define do
   factory :database_template do
-    path    'foo/index'
-    format  'html'
-    locale  'en'
-    handler 'erb'
-    partial 'false'
-    body    "something here in the body of the page: <%= 2 + 2 %>"
+    path    { 'foo/index' }
+    format  { 'html' }
+    locale  { 'en' }
+    handler { 'erb' }
+    partial { 'false' }
+    body    { "something here in the body of the page: <%= 2 + 2 %>" }
   end
 end
